89f3f1a7a0b764b461f2968e753fad243c299e3f
tiago-castro-flutteruki <tiago.castro@flutteruki.com>
2024-12-03 23:39:58 +0000

    adding missing references and added migrations on queries project

---
+++ vendingmachines.queries/vendingmachines.queries.repository/Migrations/20241203233437_vending_machines_v1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vendingmachines.queries.repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class vending_machines_v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Machines",
+                columns: table => new
+                {
+                    machineid = table.Column<string>(type: "text", nullable: false),
+                    machinetype = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Machines", x => x.machineid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProductId = table.Column<string>(type: "text", nullable: false),
+                    ProductName = table.Column<string>(type: "text", nullable: false),
+                    ProductQty = table.Column<int>(type: "integer", nullable: false),
+                    MachineId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Projects_Machines_MachineId",
+                        column: x => x.MachineId,
+                        principalTable: "Machines",
+                        principalColumn: "machineid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_MachineId",
+                table: "Projects",
+                column: "MachineId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Machines");
+        }
+    }
+}
