5634a85e318665dd11991539df59fd9c160857ea
WikiCoding <tiagomscastro@gmail.com>
2024-12-04 23:01:03 +0000

    Added semaphore to AddProduct and UpdateStock use cases so it doesn't allow concurrent calls to the server when machines are being updated

--- vendingmachines.commands/vendingmachines.commands.application/AddProductToMachine.cs
+++ vendingmachines.commands/vendingmachines.commands.application/AddProductToMachine.cs
@@ -8,22 +8,38 @@ namespace vendingmachines.commands.application;
 public class AddProductToMachine : IRequestHandler<AddProductCommand>
 {
     private readonly EventSourcingHandler _eventSourcingHandler;
+    private readonly SemaphoreSlim _semaphore;
 
     public AddProductToMachine(EventSourcingHandler eventSourcingHandler)
     {
         _eventSourcingHandler = eventSourcingHandler;
+        _semaphore = new SemaphoreSlim(1, 1);
     }
 
     public async Task Handle(AddProductCommand request, CancellationToken cancellationToken)
     {
-        var machine = await _eventSourcingHandler.GetAggregateById(request.machineId);
+        try
+        {
+            await _semaphore.WaitAsync(cancellationToken);
+            
+            var machine = await _eventSourcingHandler.GetAggregateById(request.machineId);
 
-        var productId = new ProductId(Guid.NewGuid());
-        var productName = new ProductName(request.productName);
-        var productQty = new ProductQty(request.productQty);
+            var productId = new ProductId(Guid.NewGuid());
+            var productName = new ProductName(request.productName);
+            var productQty = new ProductQty(request.productQty);
 
-        machine.AddProduct(productId, productName, productQty);
+            machine.AddProduct(productId, productName, productQty);
 
-        await _eventSourcingHandler.Save(machine);
+            await _eventSourcingHandler.Save(machine);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
     }
 }
--- vendingmachines.commands/vendingmachines.commands.application/UpdateProductStock.cs
+++ vendingmachines.commands/vendingmachines.commands.application/UpdateProductStock.cs
@@ -8,27 +8,43 @@ namespace vendingmachines.commands.application;
 public class UpdateProductStock : IRequestHandler<UpdateProductStockCommand, MachineDto>
 {
     private readonly EventSourcingHandler _eventSourcingHandler;
+    private readonly SemaphoreSlim _semaphore;
 
     public UpdateProductStock(EventSourcingHandler eventSourcingHandler)
     {
+        _semaphore = new SemaphoreSlim(1, 1);
         _eventSourcingHandler = eventSourcingHandler;
     }
 
     public async Task<MachineDto> Handle(UpdateProductStockCommand request, CancellationToken cancellationToken)
     {
-        var machine = await _eventSourcingHandler.GetAggregateById(request.aggId);
-        machine.UpdateProductStock(request.productId, request.qtyToIncrement);
-        await _eventSourcingHandler.Save(machine);
-
-        var machineProductsDto = machine.GetProducts().ToList().ConvertAll(product =>
-            new ProductDto(product.ProductId.Id.ToString(), product.ProductName.Name, product.ProductQty.qty));
-
-        return new MachineDto
+        try
         {
-            Products = machineProductsDto,
-            MachineId = machine.MachineId.Id.ToString(),
-            MachineType = machine.MachineType.Type,
-            Version = machine.Version
-        };
+            await _semaphore.WaitAsync(cancellationToken);
+            
+            var machine = await _eventSourcingHandler.GetAggregateById(request.aggId);
+            machine.UpdateProductStock(request.productId, request.qtyToIncrement);
+            await _eventSourcingHandler.Save(machine);
+
+            var machineProductsDto = machine.GetProducts().ToList().ConvertAll(product =>
+                new ProductDto(product.ProductId.Id.ToString(), product.ProductName.Name, product.ProductQty.qty));
+
+            return new MachineDto
+            {
+                Products = machineProductsDto,
+                MachineId = machine.MachineId.Id.ToString(),
+                MachineType = machine.MachineType.Type,
+                Version = machine.Version
+            };
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
     }
 }
